a59f3020053d3a7f7a16705aa5a2d49abbbeabf8
Souheib Ben Mabrouk <souheib.benmabrouk@supcom.tn>
2021-12-17 18:38:01 +0100

    first commit

---
+++ src/components/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+    <div className="logo">
+        <img src="./img/logo1.png" alt="logo" />
+        <h3>Floussek Fi Lahdha</h3>
+    </div>
+    );
+};
+
+export default Logo;
